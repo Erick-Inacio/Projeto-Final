c9faa77b6f44d5cbad2e5ab6d88c02925b2984cf
Erick F Inacio <erick.de.faria@hotmail.com>
2023-06-20 20:16:01 -0300

    pfvr agora vai

--- PContato0030282223028/PContato0030282223028/frmContato.Designer.cs
+++ PContato0030282223028/PContato0030282223028/frmContato.Designer.cs
@@ -64,6 +64,7 @@
             this.btnExcluir = new System.Windows.Forms.ToolStripButton();
             this.btnCancelar = new System.Windows.Forms.ToolStripButton();
             this.btnSair = new System.Windows.Forms.ToolStripButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.tbContato.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvContato)).BeginInit();
@@ -105,6 +106,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button1);
             this.tabPage2.Controls.Add(this.dtpData);
             this.tabPage2.Controls.Add(this.lblCidade);
             this.tabPage2.Controls.Add(this.txtEmail);
@@ -448,6 +450,15 @@
             this.btnSair.Text = "Sair";
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 217);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmContato
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -507,5 +518,6 @@
         private System.Windows.Forms.Label lblCelular;
         private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button button1;
     }
 }
